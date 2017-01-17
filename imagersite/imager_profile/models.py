880e084d174918981a2d03cb8ad9990ed4ca0027
jordan <j.schatzman@outlook.com>
2017-01-16 17:34:18 -0800

    working basic app and now adding attributes to models

--- imagersite/imager_profile/models.py
+++ imagersite/imager_profile/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.contrib.auth.models import User
+import uuid
+
+from django.db.models.signals import post_save # <-- after saving a thing, do a thing
+from django.dispatch import receiver # <-- listen for a thing to be done
 
 # Create your models here.
+
+
+class ImagerProfile(models.Model):
+    """The Library Patron and All Of Its Attributes."""
+
+    user = models.OneToOneField(
+        User,
+        related_name="profile",
+        on_delete=models.CASCADE
+    )
+    money_owed = models.DecimalField(
+        max_digits=8, decimal_places=2
+    )
+    employed = models.BooleanField(default=True)
+    address = models.CharField(max_length=255, blank=True, null=True)
+    camera_type = models.CharField(max_length=255 blank=True, null=True)
+    personal_website = models.URLField(max_length=200)
+
+
+@receiver(post_save, sender=User)
+def make_profile_for_user(sender, instance, **kwargs):
+    new_profile = PatronProfile(user=instance)
+    new_profile.money_owed = 0.0
+new_profile.save()
