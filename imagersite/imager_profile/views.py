c418a3de8aa9972658b6c09d32610501ae3f95d4
Joey DeRosa <joeyderosa11.jd@gmail.com>
2017-01-17 18:00:44 -0800

    started some stuff, login logout sortof

--- imagersite/imager_profile/views.py
+++ imagersite/imager_profile/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def home_view(request, name=''):
+    """View for the home page."""
+    return render(request, 'imagersite/home.html', context={'name': name})
--- imagersite/imagersite/urls.py
+++ imagersite/imagersite/urls.py
@@ -13,9 +13,17 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import include, url
 from django.contrib import admin
+from imager_profile.views import home_view
+from django.contrib.auth import views
+
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', home_view, name='homepage'),
+    url(r'^login/$', views.login, name='login'),
+    url(r'^logout/$', views.logout, name='logout'),
+    # url(r'^home/\w+$', home_view, name='homepage')
+    # url(r'^accounts/', include('registration.backends.hmac.urls')),
 ]
