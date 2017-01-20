814dc199e33d69f95fee6a64fe9055868c90a4e1
jordan <j.schatzman@outlook.com>
2017-01-19 17:14:20 -0800

    remove all jinja stuff

--- imagersite/imager_profile/views.py
+++ imagersite/imager_profile/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render
 # Create your views here.
 def home_view(request, name=''):
     """View for the home page."""
-    return render(request, 'imagersite/home.jinja2', context={'name': name})
+    return render(request, 'imagersite/home.html', context={'name': name})
