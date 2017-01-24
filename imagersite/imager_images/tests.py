39c187a9053d2dcc4be738b99c0447d0a1c40e36
William Benjamin Shields <ben@Williams-MacBook-Pro.local>
2017-01-24 07:03:34 -0800

    pillow added to requirements.pip. began imager_images test file. user factory, profile factory, photo factory, test class, and initial title test added.

--- imagersite/imager_images/tests.py
+++ imagersite/imager_images/tests.py
@@ -1,3 +1,41 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from imager_profile.models import ImagerProfile
+from imager_images.models import Photo, Album
+import factory
 
-# Create your tests here.
+
+class UserFactory(factory.django.DjangoModelFactory):
+    """Generate test users."""
+
+    class Meta:
+        model = User
+    username = factory.Sequence(lambda n: "User {}".format(n))
+    email = factory.LazyAttribute(
+        lambda x: "{}@imager.com".format(x.username.replace(" ", "")))
+
+
+class PhotoFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = Photo
+    title = factory.Sequence(lambda n: "Photo {}".format(n))
+
+
+class AlbumFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = Album
+    title = factory.Sequence(lambda n: "Album {}".format(n))
+
+
+class ImagesTests(TestCase):
+    """The Images App Test Runner."""
+
+    def setUp(self):
+        """User setup for tests."""
+        self.users = [UserFactory.create() for i in range(10)]
+        self.images = [PhotoFactory.create() for i in range(10)]
+        self.album = [AlbumFactory.create() for i in range(10)]
+
+    def test_photo_title(self):
+        """Test created photo models for titles."""
+        self.assertTrue("Photo" in Photo.objects.first().title)
