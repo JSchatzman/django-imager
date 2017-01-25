8604337396a7dd9492c7b8b5ee59f3aaccbd1f74
William Benjamin Shields <ben@Williams-MacBook-Pro.local>
2017-01-24 17:05:38 -0800

    library view, library template, library url.

---
+++ imagersite/imager_images/urls.py
@@ -0,0 +1,8 @@
+"""Imager images url configurations."""
+
+from django.conf.urls import url
+from imager_images.views import library_view
+
+urlpatterns = [
+    url(r'^library/$', library_view, name="library")
+]
--- imagersite/imager_images/views.py
+++ imagersite/imager_images/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from imager_images.models import Photo, Album
 
-# Create your views here.
+
+def library_view(request):
+    """View for library page."""
+    if request.user.is_authenticated():
+        albums = request.user.profile.albums.all()
+        photos = request.user.profile.photos.all()
+        return render(request, "imager_images/library.html", {
+            'albums': albums,
+            'photos': photos})
