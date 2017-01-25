51a2029853f40be847c6b1a4efb958ef18cab34d
jordan <j.schatzman@outlook.com>
2017-01-24 18:17:48 -0800

    fixing some sytnax issues

--- imagersite/imager_images/urls.py
+++ imagersite/imager_images/urls.py
@@ -1,11 +1,12 @@
 """Url patterns for imager_images."""
 
-from django.conf.utils import url
-from imager_profile.views import all_photos_view, all_albums_view, photo_view, album_view
+from django.conf.urls import url
+from imager_images.views import all_photos_view, all_albums_view, photo_view, album_view, library_view
 
 urlpatterns = [
     url(r'^photos/$', all_photos_view, name="photos"),
     url(r'^photos/(?P<pk>\d+)/$', photo_view, name="photo"),
     url(r'^albums/$', all_albums_view, name="albums"),
-    url(r'^albums/(?P<pk>\d+)/$', album_view, name="album")
+    url(r'^albums/(?P<pk>\d+)/$', album_view, name="album"),
     url(r'^library/$', library_view, name="library")
+]
