58adfe7ccc5c06d68b3eff1a39aef53b57f092cc
jordan <j.schatzman@outlook.com>
2017-01-25 18:30:00 -0800

    adding more to profile page

--- imagersite/imager_profile/views.py
+++ imagersite/imager_profile/views.py
@@ -15,11 +15,13 @@ def profile_view(request, username=None):
     user_profile = User.objects.get(username=username).profile
     #import pdb; pdb.set_trace()
     photos = Photo.objects.all().filter(photographer=user_profile.user_id)
-    # albums = Album.objects.get(owner=user_profile)
+    albums = Album.objects.all().filter(owner=user_profile)
     data = {
         'public_photos': photos.filter(published='PUBLIC'),
         'private_photos': photos.filter(published='PRIVATE'),
-        'shared_photos': photos.filter(published='SHARED')
+        'shared_photos': photos.filter(published='SHARED'),
+        'photo_count': len(photos),
+        'album_count': len(albums)
     }
     return render(request,
                   '../templates/profile.html',
